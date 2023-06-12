74b0101c8c2f8de39a6d3236c8bbbfb45303a640
nGoretti <nanygoretti@gmail.com>
2023-06-12 13:49:40 -0700

    Added Smooth Scrolling

--- js/index.js
+++ js/index.js
@@ -113,6 +113,25 @@ $(document).ready(function(){
                 $("#phoneButton").css("cursor", "default");
             }   
         }
-    )
+    ),
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+                }, 500, function(){
+        
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
     
 });
